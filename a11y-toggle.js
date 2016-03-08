6d5038eaee9cb929d4568e1c3c02060d0aa67055
Sherri Alexander <salexander@cgiinteractive.com>
2016-03-08 14:39:15 -0500

    Another error check on the document click event listener

--- a11y-toggle.js
+++ a11y-toggle.js
@@ -1,31 +1,33 @@
 (function () {
   var toggles = document.querySelectorAll('[data-a11y-toggle][aria-controls]');
-  
-  if (toggles.length > 0) {
-    var targetsList = [];
 
-    for (var i = 0; i < toggles.length; i += 1) {
-      var toggle = toggles[i];
-      targetsList.push('#' + toggle.getAttribute('aria-controls'));
-      toggle.hasAttribute('aria-expanded') || toggle.setAttribute('aria-expanded', false);
-    }
+  var targetsList = [];
 
-    var targets = document.querySelectorAll(targetsList);
-    var targetsMap = {};
+  for (var i = 0; i < toggles.length; i += 1) {
+    var toggle = toggles[i];
+    targetsList.push('#' + toggle.getAttribute('aria-controls'));
+    toggle.hasAttribute('aria-expanded') || toggle.setAttribute('aria-expanded', false);
+  }
 
-    for (var j = 0; j < targets.length; j += 1) {
-      var target = targets[j];
-      targetsMap[target.id] = target;
-      target.hasAttribute('aria-hidden') || target.setAttribute('aria-hidden', true);
-    }
+  //var targets = document.querySelectorAll(targetsList);
+  var targets = targetsList.length && document.querySelectorAll(targetsList);
+  var targetsMap = {};
 
-    document.addEventListener('click', function (event) {
-      var toggle = event.target;
+  for (var j = 0; j < targets.length; j += 1) {
+    var target = targets[j];
+    targetsMap[target.id] = target;
+    target.hasAttribute('aria-hidden') || target.setAttribute('aria-hidden', true);
+  }
+
+  document.addEventListener('click', function (event) {
+    var toggle = event.target;
+
+    if (toggle.hasAttribute('aria-controls')) {
       var target = targetsMap[toggle.getAttribute('aria-controls')];
       var isExpanded = JSON.parse(toggle.getAttribute('aria-expanded'));
 
       toggle.setAttribute('aria-expanded', !isExpanded);
       target.setAttribute('aria-hidden', isExpanded);
-    });
-  }
+    }
+  });
 }());
